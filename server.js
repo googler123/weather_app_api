b591a29b2d96927b14776257e58270daa1c5ecfd
Vinamra Vinayak  Tandon <48364487+googler123@users.noreply.github.com>
2019-05-12 22:37:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,6 @@ app.get('/', function (req, res) {
 app.post('/', function (req, res) {
   let city = req.body.city;
   let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
-
   request(url, function (err, response, body) {
     if(err){
       res.render('index', {weather: null, error: 'Error, please try again'});
