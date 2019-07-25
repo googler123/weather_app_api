5b78ff5ffdb1146d86e7c0aa25c60aaa908c4024
Vinamra Vinayak  Tandon <48364487+googler123@users.noreply.github.com>
2019-07-25 18:45:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const request = require('request');
 const app = express();
 
-const apiKey = '52810d6ba4877f6d6f34e93f7ab60175';
+const apiKey ='52810d6ba4877f6d6f34e93f7ab60175';
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
